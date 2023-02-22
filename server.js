a87ad796651afdefd6dfc3be99570d4a5b299004
azizsnoussi <63208771+azizsnoussi@users.noreply.github.com>
2023-02-22 20:44:18 +0100

    seance 3

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
-var http = require('http');
-var url = require('url');
-var querystring = require('querystring');
+const express = require('express');
 
-var server = http.createServer(function(req,res)
-{
-    var params = querystring.parse(url.parse(req.url).query);
-    res.writeHead(200,{"Content-Type":"text/plain"});
-    if('id' in params && 'login' in params) {
-        res.write('votre id est '+ params['id']+
-        ' et votre login '+params['login']);
-    }
-    else
-    {
-        res.writeHead(404,{"Content-Type":"text/plain"});
-        res.write('Not found');
-    }
-    res.end();
+var app = express();
+
+
+
+app.get('/test',(req,res,next)=>{
+console.log("Hello",req);
+res.json({message: " hello"});
 });
-server.listen(8080);
 
+
+
+app.use('/test',(req,res,next)=>{
+    console.log("Hello",req);
+    res.json({message: " hello"});
+    });
+
+app.listen(8080);
